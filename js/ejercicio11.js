fc0b3fdc3acc672046c33739a075f66133cb3414
JulietaNF <julietanferreyra@yahoo.com.ar>
2023-08-08 03:35:11 -0300

    task complete

--- js/ejercicio11.js
+++ js/ejercicio11.js
@@ -9,3 +9,37 @@ Output: El 20 es divisible por 2 y por 5.
 Output: El 210 es divisible por 2, por 3, por 5 y por 7.
 */
 
+let numero = parseInt(prompt("Pon un número:"));
+
+   
+    if (Number.isInteger(numero / 2) && Number.isInteger(numero / 3) && Number.isInteger(numero / 5) && Number.isInteger(numero / 7)){
+        document.write("El " + numero + " es divisible por 2, por 3, por 5 y por 7.");
+    } else if (Number.isInteger(numero / 2) && Number.isInteger(numero / 3) && Number.isInteger(numero / 5)){
+        document.write("El " + numero + " es divisible por 2, por 3 y por 5.");
+    } else if (Number.isInteger(numero / 2) && Number.isInteger(numero / 3)){        
+        document.write("El " + numero + " es divisible por 2 y por 3.");
+    } else if (Number.isInteger(numero / 2)){
+        if (Number.isInteger(numero / 2) && Number.isInteger(numero / 5)){
+            document.write("El " + numero + " es divisible por 2 y por 5.");
+        }else if (Number.isInteger(numero / 7) && Number.isInteger(numero / 2)){
+            document.write("El " + numero + " es divisible por 2 y por 7.");
+        } else {
+            document.write("El " + numero + " es divisible por 2");
+        }
+    }else if (Number.isInteger(numero / 5)){
+        if(Number.isInteger(numero / 5) && Number.isInteger(numero / 3)){
+            document.write("El " + numero + " es divisible por 3 y por 5.");
+        } else {
+            document.write("El " + numero + " es divisible por 5.");
+        }
+    } else if (Number.isInteger(numero / 3)){
+        if(Number.isInteger(numero / 3) && Number.isInteger(numero / 7)){
+            document.write("El " + numero + " es divisible por 3 y por 7.");
+        } else{
+            document.write("El " + numero + " es divisible por 3.");
+        }
+    } else if (Number.isInteger(numero / 7)){
+        document.write("El " + numero + " es divisible por 7.");
+    } else{
+        document.write("El número " + numero + " no es divisible");
+    }
